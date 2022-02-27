9949ed8262c4c1430224c020f373ba6a71f50ce6
Gerry Wolfe <gwolfe@slync.io>
2022-02-27 10:16:18 -0800

    addition

--- programs/mycalculatordapp/src/lib.rs
+++ programs/mycalculatordapp/src/lib.rs
@@ -17,6 +17,10 @@ pub mod mycalculatordapp {
 
     pub fn add(ctx:Context<Addition>, num1: i64, num2: i64) -> ProgramResult {
 
+        let calculator = &mut ctx.accounts.calculator;
+        calculator.result = num1 + num2;
+        Ok(())
+
     }
 
     pub fn multiply(ctx: Context<Multiplication>, num1: i64, num2: i64) -> ProgramResult {
@@ -32,15 +36,15 @@ pub mod mycalculatordapp {
     }
 }
 
+#[derive(Accounts)]
+pub struct Addition<'info> {
+    #[account(mut)]
+    pub calculator: Account<'info, Calculator>,
+}
+
 #[account]
 pub struct Calculator {
     pub greeting: String,
     pub result: i64,
     pub remainder: i64,
-}
-
-#[derive(Accounts)]
-pub struct Addition<'info> {
-    #[account(mut)]
-    pub calculator: Account<'info, Calculator>,
 }
