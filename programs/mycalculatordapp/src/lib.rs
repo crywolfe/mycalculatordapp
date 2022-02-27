968964e8786b7e318f7cecaabda73180aa725d6c
Gerry Wolfe <gwolfe@slync.io>
2022-02-27 10:21:17 -0800

    remaining operations

--- programs/mycalculatordapp/src/lib.rs
+++ programs/mycalculatordapp/src/lib.rs
@@ -25,19 +25,46 @@ pub mod mycalculatordapp {
 
     pub fn multiply(ctx: Context<Multiplication>, num1: i64, num2: i64) -> ProgramResult {
 
+        let calculator = &mut ctx.accounts.calculator;
+        calculator.result = num1 * num2;
+        Ok(())
     }
 
     pub fn subtract(ctx: Context<Subtraction>, num1: i64, num2: i64) -> ProgramResult {
-
+        
+        let calculator = &mut ctx.accounts.calculator;
+        calculator.result = num1 - num2;
+        Ok(())
     }
 
     pub fn divide(ctx: Context<Division>, num1: i64, num2: i64) -> ProgramResult {
-
+        let calculator = &mut ctx.accounts.calculator;
+        calculator.result = num1 / num2;
+        calculator.remainder = num1 % num2;
+        Ok(())
     }
 }
 
 #[derive(Accounts)]
 pub struct Addition<'info> {
+    #[account(mut)]
+    pub calculator: Account<'info, Calculator>,
+}
+
+#[derive(Accounts)]
+pub struct Multiplication<'info> {
+    #[account(mut)]
+    pub calculator: Account<'info, Calculator>,
+}
+
+#[derive(Accounts)]
+pub struct Subtraction<'info> {
+    #[account(mut)]
+    pub calculator: Account<'info, Calculator>,
+}
+
+#[derive(Accounts)]
+pub struct Division<'info> {
     #[account(mut)]
     pub calculator: Account<'info, Calculator>,
 }
